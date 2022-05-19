4f6ac90d4723551b297ea4e0fefb7fbeeb85a22b
Crash27coding <99522377+mohit2202solanki@users.noreply.github.com>
2022-05-19 12:24:33 +0530

    Add files via upload
    Programs by Mohit

---
+++ normal_swap.py
@@ -0,0 +1,11 @@
+a = int( input("Please enter value for a: "))  
+b = int( input("Please enter value for b: "))  
+   
+# To swap the value of two variables  
+
+temp = a
+a = b  
+b = temp
+   
+print ("The Value of a after swapping: ", a)  
+print ("The Value of b after swapping: ", b)  
---
+++ swap_without_3rd.py
@@ -0,0 +1,8 @@
+a = int( input("Please enter value for a: "))  
+b = int( input("Please enter value for b: "))  
+   
+# To Swap the values of two variables  
+a, b = b, a  
+   
+print ("The Value of a after swapping: ", a)  
+print ("The Value of b after swapping: ", b)  
